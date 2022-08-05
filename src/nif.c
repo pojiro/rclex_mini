7f4f89da8175b7ee7a5f775aceab81f55258e43c
Ryota Kinukawa <kinukawa@tombo-works.com>
2022-08-05 07:27:19 +0000

    Fix bug, add rcl_init()

--- src/nif.c
+++ src/nif.c
@@ -4,25 +4,33 @@
 ErlNifResourceType* rt_node;
 
 static ERL_NIF_TERM create_node(ErlNifEnv *env, int argc, const ERL_NIF_TERM argv[]){
- rcl_context_t context = rcl_get_zero_initialized_context();
- // ... initialize the context with rcl_init()
- rcl_node_t node = rcl_get_zero_initialized_node();
- rcl_node_options_t node_ops = rcl_node_get_default_options();
- // ... node options customization
- rcl_ret_t ret = rcl_node_init(&node, "node_name", "/node_ns", &context, &node_ops);
- // ... error handling and then use the node, but eventually deinitialize it:
- ret = rcl_node_fini(&node);
+	rcl_ret_t ret;
 
- return enif_make_atom(env, "ok");
+	rcl_context_t context = rcl_get_zero_initialized_context();
+
+	rcl_init_options_t options = rcl_get_zero_initialized_init_options();
+	ret = rcl_init_options_init(&options, rcutils_get_default_allocator());
+
+	ret = rcl_init(0, NULL, &options, &context);
+	ret = rcl_init_options_fini(&options);
+
+	rcl_node_t node = rcl_get_zero_initialized_node();
+	rcl_node_options_t node_ops = rcl_node_get_default_options();
+	ret = rcl_node_init(&node, "node_name", "/node_ns", &context, &node_ops);
+
+	printf("%s\n", rcl_node_get_name(&node));
+
+	ret = rcl_node_fini(&node);
+	return enif_make_atom(env, "ok");
 }
 
 static ERL_NIF_TERM hello(ErlNifEnv *env, int argc, const ERL_NIF_TERM argv[]){
-  return enif_make_atom(env, "hello");
+	return enif_make_atom(env, "hello");
 }
 
 static ErlNifFunc nif_funcs[] = {
-  {"create_node", 0, create_node, 0},
-  {"hello", 0, hello, 0}
+	{"create_node", 0, create_node, 0},
+	{"hello", 0, hello, 0}
 };
 
 ERL_NIF_INIT(Elixir.RclexMini.Nif, nif_funcs, NULL, NULL, NULL, NULL)
